9607a3461c2bfc8773e506e6b7e91de241b2a1bd
Febi Anggun Lestari <febicisasmi76@gmail.com>
2025-12-29 10:32:36 +0700

    Update kontak.py

--- kontak.py
+++ kontak.py
@@ -3,6 +3,6 @@ def contact_me():
     st.write('**Contact Me**')
     st.write('Jika Anda memiliki pertanyaan atau ingin berdiskusi lebih lanjut, silakan hubungi saya melalui email atau media sosial berikut:')
     st.write('📧 Email: vebbyanggunlestari@gmail.com ')
-    st.write('🔗 LinkedIn: [linkedin.com/in/syahrul-septian-zein](https://www.linkedin.com/in/syahrul-septian-zein/)')
+    st.write('🔗 LinkedIn: [linkedin.com/in/febi-anggun-lestari](https://www.linkedin.com/in/febi-anggun-lestari-8026422b0/)')
     st.write('🐱 GitHub: [github.com/febicisasmi76](https://github.com/febicisasmi76-ai/porto-unimus1)')
     st.write('Saya terbuka untuk kolaborasi dan diskusi seputar data science dan machine learning. Terima kasih telah mengunjungi aplikasi ini!')
